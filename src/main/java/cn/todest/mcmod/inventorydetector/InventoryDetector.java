7fbe3db2588cbec1beb7d9d78954d5eed3ced3c4
todest <1123792492@qq.com>
2021-11-21 23:56:27 +0800

    fix GUI

--- src/main/java/cn/todest/mcmod/inventorydetector/InventoryDetector.java
+++ src/main/java/cn/todest/mcmod/inventorydetector/InventoryDetector.java
@@ -1,7 +1,7 @@
 package cn.todest.mcmod.inventorydetector;
 
 import cn.todest.mcmod.inventorydetector.common.events.DetectorEvents;
-import net.minecraft.init.Blocks;
+import cn.todest.mcmod.inventorydetector.common.events.RecordGuiEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
@@ -14,8 +14,7 @@ public class InventoryDetector {
 
     @EventHandler
     public void init(FMLInitializationEvent event) {
-        // some example code
-        System.out.println("DIRT BLOCK >> " + Blocks.dirt.getUnlocalizedName());
+        MinecraftForge.EVENT_BUS.register(new RecordGuiEvent());
         MinecraftForge.EVENT_BUS.register(new DetectorEvents());
     }
 }
